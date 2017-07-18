3dedb5040e6a34cbec29d8008e4fa6a93e1aaf5d
姜帅杰 <1830702951@qq.com>
2017-07-18 13:30:30 +0800

    Initial Commit

---
+++ LoginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginDemo
+//
+//  Created by wangrui on 2017/7/18.
+//  Copyright © 2017年 codeteenager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginDemo
+//
+//  Created by wangrui on 2017/7/18.
+//  Copyright © 2017年 codeteenager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
